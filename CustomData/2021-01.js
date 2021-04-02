d6695738cda5640260768297e77145e0fc2e1f21
STR-HK <78652371+STR-HK@users.noreply.github.com>
2021-04-02 21:26:18 +0900

    Update 2021-01.js

--- CustomData/2021-01.js
+++ CustomData/2021-01.js
@@ -3,8 +3,7 @@
 
 // Insert Checks Like This Form.
 // Ex) Date:Count
-var Checks = ['1:1','3:1','4:1','5:1','7:1','8:1',
-                '11:1','13:1','15:1','17:1','18:1','28:2']
+var Checks = ['1:1','3:1','4:1','5:1','7:1','8:1','11:1','13:1','15:1','17:1','18:1','28:2']
 
 // Call Import Function. ex) Import()
 // If You Inputed 'Checks' => Import(Checks)
