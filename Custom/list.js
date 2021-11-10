ca2549f675a2ffed60b88f2afc8556794d82c13c
STR-HK <78652371+STR-HK@users.noreply.github.com>
2021-11-10 15:40:20 +0900

    add 2021-10

--- Custom/list.js
+++ Custom/list.js
@@ -1,5 +1,5 @@
 // Write All of Your Data JavaScript File's Name.
-let array = ['2021-01','2021-02','2021-03','2021-04','2021-05','2021-06','2021-07','2021-08','2021-09']
+let array = ['2021-01','2021-02','2021-03','2021-04','2021-05','2021-06','2021-07','2021-08','2021-09','2021-10']
 
 // Don't Touch Here.
 // This Code Will Automatically Imports Written JavaScript Files In Html.
